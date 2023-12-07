9a8290e851a916ccdc0037937a188b030bddf90b
dbrechka <unigineprojects@mail.ru>
2023-12-07 07:12:29 +0400

    Calculator 2nd take

--- 1_Calculator_Chapter_6/main.cpp
+++ 1_Calculator_Chapter_6/main.cpp
@@ -3,18 +3,43 @@
 using namespace std;
 
 int main() {
-  cout << "Input the expression (+ and - allowed): ";
+  cout << "Input the expression (+, -, * and / allowed): ";
   int l_val = 0;
   int r_val;
   char operation;
-  int result;
-  cin >> l_val >> operation >> r_val;
 
-  if (operation == '+')
-    result = l_val + r_val;
-  else if (operation == '-')
-    result = l_val - r_val;
-
-  cout << "Result:" << result << '\n';
+  cin >> l_val;
+  if (!cin) {
+    cout << "No first operand" << endl;
+    return 0;
+  };
+  while (cin >> operation) {
+    // This if is fixing error in the book.
+    // Show result when '=' is entered.
+    if (operation == '=') {
+      cout << l_val << endl;
+      return 0;
+    }
+    cin >> r_val;
+    if (!cin) {
+      cout << "No second operand" << endl;
+      return 0;
+    }
+    switch (operation) {
+      case '+':
+        l_val += r_val;
+        break;
+      case '-':
+        l_val -= r_val;
+        break;
+      case '*':
+        l_val *= r_val;
+        break;
+      case '/':
+        l_val /= r_val;
+        break;
+    }
+  }
+  cout << "Incorrect expression" << endl;
   return 0;
 }
