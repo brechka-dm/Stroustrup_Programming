4385a8fa5d0cc7e2201bb7a449e71261fb2983a3
dbrechka <unigineprojects@mail.ru>
2023-12-07 06:26:25 +0400

    Calculator 1st take

--- 1_Calculator_Chapter_6/main.cpp
+++ 1_Calculator_Chapter_6/main.cpp
@@ -2,4 +2,19 @@
 
 using namespace std;
 
-int main() { cout << "Hello"; }
+int main() {
+  cout << "Input the expression (+ and - allowed): ";
+  int l_val = 0;
+  int r_val;
+  char operation;
+  int result;
+  cin >> l_val >> operation >> r_val;
+
+  if (operation == '+')
+    result = l_val + r_val;
+  else if (operation == '-')
+    result = l_val - r_val;
+
+  cout << "Result:" << result << '\n';
+  return 0;
+}
