bc91849d947ba2d710b9a24193d49e0160490136
dbrechka <unigineprojects@mail.ru>
2023-12-13 07:12:30 +0400

    Complete

--- 4_Chapter_6_Exercises/5/main.cpp
+++ 4_Chapter_6_Exercises/5/main.cpp
@@ -8,17 +8,28 @@
 
 using namespace std;
 
+// Sets described in Chapter 21.
 set<string> conjunctions = {"and", "or", "but"};
 set<string> nouns = {"birds", "fish", "c++"};
 set<string> verbs = {"rules", "fly", "swim"};
+set<string> articles = {"the"};
 
+// We use this function to convert the letters in a word to lower case, so that
+// we don't have to process capital and small letter words separately.
 string strToLowerCopy(string const& str) {
+  // We make a copy of the function argument because the string changes inside
+  // the function, but we don't want the string passed to the function by
+  // reference to change.
   string strCopy = str;
+
+  // transform described in:
+  // https://en.cppreference.com/w/cpp/algorithm/transform
   transform(strCopy.begin(), strCopy.end(), strCopy.begin(),
             [](unsigned char c) { return tolower(c); });
   return strCopy;
 }
 
+// Auxiliary Function. Determines whether the word is in the wordSet.
 bool isStringInSet(string const& word, set<string> const& wordSet) {
   string lowerWord = strToLowerCopy(word);
   return wordSet.find(lowerWord) != wordSet.end();
@@ -32,17 +43,36 @@ bool isNoun(string const& word) { return isStringInSet(word, nouns); }
 
 bool isVerb(string const& word) { return isStringInSet(word, verbs); }
 
+bool isArticle(string const& word) { return isStringInSet(word, articles); }
+
+bool isNounWithArticle(vector<string> const& words) {
+  // There should be 2 words: article and noun.
+  if (words.size() < 2) return false;
+  return isArticle(words[0]) && isNoun(words[1]);
+}
+
 bool isSimpleSentence(vector<string> const& words) {
-  return isNoun(words[0]) && isVerb(words[1]);
+  // There may be 2 or 3 words: noun and verb or article, noun and verb.
+  switch (words.size()) {
+    case 2:
+      return isNoun(words[0]) && isVerb(words[1]);
+    case 3:
+      return isNounWithArticle({words[0], words[1]}) && isVerb(words[2]);
+  }
+  return false;
 }
 
 bool isSentence(string const& str) {
   if (str.length() < 2) return 0;
 
+  // stringstream described in Chapter 23. It will help us to split str to
+  // separate words.
   stringstream stream(str);
   vector<string> words;
   string word;
   bool sentenceFound = false;
+
+  // While there words in string.
   while (stream >> word) {
     if (sentenceFound && isConjunction(word)) {
       words.clear();
@@ -50,10 +80,9 @@ bool isSentence(string const& str) {
       continue;
     }
     words.push_back(word);
-    if (words.size() == 2) {
-      if (isSimpleSentence(words)) sentenceFound = true;
-      else return false;
-    }
+    if (words.size() > 1 && words.size() <= 3 && isSimpleSentence(words))
+      sentenceFound = true;
+    if (words.size() > 3) return false;
   }
   return sentenceFound;
 }
@@ -61,6 +90,6 @@ bool isSentence(string const& str) {
 int main() {
   string input;
   getline(cin, input);
-  cout << isSentence(input);
+  cout << boolalpha << isSentence(input);
   return 0;
 }
