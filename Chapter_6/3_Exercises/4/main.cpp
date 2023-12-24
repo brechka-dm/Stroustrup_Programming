9ec72279b84424d6e132bc5dcb48421991bf16e2
dbrechka <unigineprojects@mail.ru>
2023-12-24 07:52:11 +0400

    Ex 4 add comment

--- Chapter_6/3_Exercises/4/main.cpp
+++ Chapter_6/3_Exercises/4/main.cpp
@@ -4,8 +4,24 @@
 
 using namespace std;
 
+/*
+ * This program is the solution to Exercise 4 of Chapter 6 of PROGRAMMING.
+ * Principles and Practice Using C++. 2nd edition.
+ * The program takes as input a set of pairs consisting of a name
+ * and a value, e.g. Joe 17 and Barbara 22. Input stops when the string NoName 0
+ * is entered. If the name is entered twice, the program displays an error
+ * message. When input is complete, the program prints all pairs (name, score)
+ * one per line. The NameValue class is used to store the data. The class is
+ * storing the string and the value.
+ */
+
+/*
+ * This is analogous to the function error used in the book.
+ */
 void error(const string& msg) {
   cout << msg;
+
+  // Using exit to terminate program with return value of 1.
   exit(1);
 }
 
@@ -16,11 +32,19 @@ class NameValue {
  public:
   NameValue() : pName(""), pValue(0) {}
   NameValue(const string& name, int value) : pName(name), pValue(value) {}
-  bool operator==(const NameValue& other) const { return pName == other.pName; }
+  // Use this method to print values.
   void print() const { cout << pName << ": " << pValue << endl; }
+  // This is an overloading of the = operator to be able to compare NameValue
+  // objects with each other (See Chapter 9.6). In this case, the comparison is
+  // done by the pName field.
+  bool operator==(const NameValue& other) const { return pName == other.pName; }
 };
 
+/*
+ * Returns true if val is inside container.
+ */
 bool valInContainer(const NameValue& val, const vector<NameValue>& container) {
+  // See Chapter 21.2.
   const auto containerIter = find(container.begin(), container.end(), val);
   return containerIter != container.end();
 }
