ad981deeef96b105f3d82da1f3d3d2110245a186
dbrechka <unigineprojects@mail.ru>
2024-01-23 06:35:11 +0400

    Ch8 task 1

---
+++ Chapter_8/Tasks/1/my.cpp
@@ -0,0 +1,12 @@
+#include "my.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int foo; // definition.
+
+void print_foo() { cout << foo << endl; }
+
+void print(int i) { cout << i << endl; }
---
+++ Chapter_8/Tasks/1/use.cpp
@@ -0,0 +1,13 @@
+#include "my.h"
+
+/*
+ * This program is solution of Task 1 from Chapter 8.
+ */
+
+int main() {
+  foo = 7;
+  print_foo();
+  print(99);
+
+  return 0;
+}
