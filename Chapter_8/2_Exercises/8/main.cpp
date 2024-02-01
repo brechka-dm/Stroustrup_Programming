134a5cf480acb97bbe4f1bc0d80db7c1f1e9df9d
dbrechka <unigineprojects@mail.ru>
2024-02-01 06:35:28 +0400

    Ex8 add comments

--- Chapter_8/2_Exercises/8/main.cpp
+++ Chapter_8/2_Exercises/8/main.cpp
@@ -6,7 +6,29 @@
 
 using namespace std;
 
+/*
+ * This program is a solution of the Exercise 8 and 9 from the Chapter 8.
+ * 8. "Write a simple randint() function that generates pseudo-random numbers in
+ * the range [0:MAXINT]."
+ * 9. "Write a function that computes a pseudorandom integer in the range [a:b):
+ * rand_in_range(int a, int b) using the randint() function from the previous
+ * exercise."
+ *
+ * The solution uses the formula for the linear congruent generator described in
+ * https://habr.com/ru/articles/499490/.
+ *
+ * Implementation of functions for number generation is placed in a separate
+ * file.
+ *
+ * The maxrand() function returns the maximum random number that can be
+ * generated (analog of MAXINT from the task).
+ *
+ * The main() function uses the rand() function from the <cstdlib> standard
+ * library to compare results.
+ */
+
 int main() {
+  // Using value of the time as a random seed.
   const long long seed{time(0)};
   srand(seed);
   ex8::set_seed(seed);
--- Chapter_8/2_Exercises/8/rand.cpp
+++ Chapter_8/2_Exercises/8/rand.cpp
@@ -1,6 +1,8 @@
 #include "rand.h"
 
 namespace {
+// The values of the numbers are taken from the article
+// https://habr.com/ru/articles/499490/
 constexpr unsigned long a{1103515245};
 constexpr unsigned long c{12345};
 constexpr unsigned long m{32768};  // 2^15
@@ -9,11 +11,13 @@ constexpr unsigned long n{65536};  // 2^16
 static unsigned long int next = 1;
 }  // namespace
 
+unsigned long maxrand() { return m - 1; }
+
 void ex8::set_seed(unsigned long int seed) { next = seed; }
 
-int ex8::rand() {
+int ex8::randint() {
   next = next * a + c;
   return static_cast<int>((next / n) % m);
 }
 
-int ex8::rand_in_range(int a, int b) { return a + rand() % (b - a); }
+int ex8::rand_in_range(int a, int b) { return a + randint() % (b - a); }
